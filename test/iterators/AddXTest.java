0f1037838f5a33206335051fa47a87a8a284d01f
Brandon Myers <brandon-d-myers@uiowa.edu>
2017-10-03 13:17:55 -0500

    add1 -> addx

---
+++ test/iterators/AddXTest.java
@@ -0,0 +1,59 @@
+
+package iterators;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AddXTest {
+    
+    public AddXTest() {
+    }
+    
+		@Test
+		public void emptyTest() {
+			Integer[] inputValues = {};
+			List<Integer> input = Arrays.asList(inputValues);
+                        AddXTest op = new AddXTest(1, input.iterator());
+			assertEquals(false, op.hasNext());
+			assertEquals(false, op.hasNext());
+		}
+		
+		@Test
+		public void oneTest() {
+			Integer[] inputValues = {100};
+			List<Integer> input = Arrays.asList(inputValues);
+                         AddXTest op = new AddXTest(10, input.iterator());
+			assertEquals(true, op.hasNext());
+			assertEquals(true, op.hasNext());
+			assertEquals(110, (int)op.next());
+			assertEquals(false, op.hasNext());
+			assertEquals(false, op.hasNext());
+		}
+		
+		@Test
+		public void moreTest() {
+			Integer[] inputValues = {10,50,1,400};
+			List<Integer> input = Arrays.asList(inputValues);
+			//Apply<Integer,Integer> op = new Apply<>(new TimesTwo(), input.iterator());
+                        AddXTest op = new AddXTest(10, input.iterator());
+
+			assertEquals(true, op.hasNext());
+			assertEquals(true, op.hasNext());
+			assertEquals(20, (int)op.next());
+			assertEquals(true, op.hasNext());
+			assertEquals(true, op.hasNext());
+			assertEquals(true, op.hasNext());
+			assertEquals(60, (int)op.next());
+			assertEquals(12, (int)op.next());
+			assertEquals(410, (int)op.next());
+			assertEquals(false, op.hasNext());
+		}
+		
+		
+
+
+		
+    
+}
