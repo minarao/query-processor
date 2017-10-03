6c402217de3bfb3957458deaa30cce8a2868c0e6
Alsulaiman, Thamer <thamer-alsulaiman@uiowa.edu>
2017-10-03 00:16:26 -0500

    Delete FlatApplyFunction.java

--- FlatApplyFunction.java
+++
@@ -1,9 +0,0 @@
-package iterators;
-
-
-import java.util.List;
-
-// Interface for classes to define a function from InT to List<OutT>
-public interface FlatApplyFunction<InT, OutT> {
-		public List<OutT> apply(InT x);
-}
